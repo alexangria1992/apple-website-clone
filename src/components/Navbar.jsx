c92023c814e38fb42448dd1b2884de00312871fa
Alexandre Angrignon <alex040892@gmail.com>
2024-05-18 21:29:56 +1200

    Navbar Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import styles from './Navbar.module.css';
+import { appleImg } from '../utils';
+import { searchImg } from '../utils';
+import { bagImg } from '../utils';
+import { navLists } from '../constants';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className={styles.navbarHeader}>
+      <nav className={styles.navbarItems}>
+        <img src={appleImg} alt='' width={14} height={18} />
+        <div className={styles.navbarLinksWrapper}>
+          {navLists.map((nav) => (
+            <div key={nav} className={styles.navbarLinks}>
+              {nav}
+            </div>
+          ))}
+        </div>
+
+        <div className={styles.navbarIcons}>
+          <img src={searchImg} alt='' width={18} height={18} />
+          <img src={bagImg} alt='' width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
